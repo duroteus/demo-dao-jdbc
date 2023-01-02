144f0f70b1c764d006596adc8980558e86461452
gstv <gstv.mra@gmail.com>
2023-01-02 13:00:26 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/App.java
+++ src/application/App.java
@@ -2,6 +2,8 @@ package application;
 
 import java.time.LocalDate;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,9 @@ public class App {
 
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
     }
 }
