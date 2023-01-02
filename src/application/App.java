768a04256071bfa424e9ff50e9a17d9c623498b8
gstv <gstv.mra@gmail.com>
2023-01-02 13:18:38 -0300

    findById implementation

--- src/application/App.java
+++ src/application/App.java
@@ -1,19 +1,15 @@
 package application;
 
-import java.time.LocalDate;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,14 +1,14 @@
 package model.entities;
 
 import java.io.Serializable;
-import java.time.LocalDate;
+import java.util.Date;
 
 public class Seller implements Serializable {
 
     private Integer id;
     private String name;
     private String email;
-    private LocalDate birthDate;
+    private Date birthDate;
     private Double baseSalary;
 
     private Department department;
@@ -16,7 +16,7 @@ public class Seller implements Serializable {
     public Seller() {
     }
 
-    public Seller(Integer id, String name, String email, LocalDate birthDate, Double baseSalary,
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary,
             Department department) {
         this.id = id;
         this.name = name;
@@ -50,11 +50,11 @@ public class Seller implements Serializable {
         this.email = email;
     }
 
-    public LocalDate getBirthDate() {
+    public Date getBirthDate() {
         return birthDate;
     }
 
-    public void setBirthDate(LocalDate birthDate) {
+    public void setBirthDate(Date birthDate) {
         this.birthDate = birthDate;
     }
 
