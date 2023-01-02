740026397c4ffea529c48f65cf52dc860a2f29f6
gstv <gstv.mra@gmail.com>
2023-01-02 12:51:53 -0300

    Seller entity class

--- src/application/App.java
+++ src/application/App.java
@@ -1,11 +1,15 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj);
+        System.out.println(seller);
     }
 }
