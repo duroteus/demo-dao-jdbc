eb2197c883d97418d5bae2d9d8645718f6d21217
gstv <gstv.mra@gmail.com>
2023-01-10 09:46:47 -0300

    delete implementation

--- src/application/App.java
+++ src/application/App.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -10,6 +11,8 @@ import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -41,5 +44,12 @@ public class App {
         sellerDao.update(seller);
         System.out.println("Update completed");
 
+        System.out.println("\n=== TEST 6: seller Delete ===");
+        System.out.print("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+
+        sc.close();
     }
 }
