e6da5477d4b8bb6bf548ceeb94c8ec918ff3043f
gstv <gstv.mra@gmail.com>
2023-01-10 09:25:00 -0300

    findAll implementation

--- src/application/App.java
+++ src/application/App.java
@@ -16,10 +16,15 @@ public class App {
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
-        System.out.println("\n=== TEST 1: seller findByDepartmentId ===");
+        System.out.println("\n=== TEST 2: seller findByDepartmentId ===");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
 
+        System.out.println("\n=== TEST 3: seller findAll ===");
+        list = sellerDao.findAll();
         for (Seller obj : list) {
             System.out.println(obj);
         }
