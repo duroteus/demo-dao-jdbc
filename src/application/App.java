799262c231870c14e92ef612f00ec1cecba4c0a9
gstv <gstv.mra@gmail.com>
2023-01-10 09:41:23 -0300

    update implementation

--- src/application/App.java
+++ src/application/App.java
@@ -35,5 +35,11 @@ public class App {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+        System.out.println("\n=== TEST 5: seller Update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+
     }
 }
